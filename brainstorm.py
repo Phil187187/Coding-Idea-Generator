fd7944424747e7b742218de247a2b992688408a9
Philipp Stoboy <phil_stb@icloud.com>
2023-04-04 16:28:36 +0200

    first commit

---
+++ brainstorm.py
@@ -0,0 +1,56 @@
+import openai
+#import argparse
+import webbrowser
+
+data_dir = "C:/Users/Philipp/Desktop/Project Idea generator/data/"
+
+#p = argparse.ArgumentParser(description='Brainstorm idea')
+
+#p.add_argument('--idea', type=str, required=True, help='Idea suggestions')
+
+#args = p.parse_args()
+
+def open_file(file_name):
+    with open(data_dir + file_name, 'r') as f:
+        return f.read()
+    
+def write_file(file_name, text):
+    with open(data_dir + file_name, 'w') as f:
+        f.write(text)
+        return
+
+#open html file in default browser
+def open_html(file_name):
+    with open(data_dir + file_name, 'r') as f:
+        ideas_html = f.read()
+    return ideas_html
+
+
+openai.api_key = open_file('api_key.txt')
+
+sys_prompt = open_file('prompt.txt')
+
+assistant_reply_best = open_file('assistant_reply_best.txt')
+
+def brainstorm_html_idea_and_write(idea):
+    output = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": sys_prompt},
+            {"role": "user", "content": "brainstorm --idea ai --output HTML"},
+            {"role": "assistant", "content": assistant_reply_best},
+            {"role": "user", "content": f"brainstorm --idea {idea} --output HTML"}
+        ]
+    )
+    #print(output.choices[0].message.content)
+    write_file(f'{idea}.html', output.choices[0].message.content)
+    return output.choices[0].message.content
+
+
+#def main(idea):
+    #return brainstorm_html_idea_and_write(idea)
+
+#if __name__ == '__main__':
+    #brainstorm_html_idea_and_write(args.idea)
+    #open_html(f'{args.idea}.html')
+    #main(args.idea)
